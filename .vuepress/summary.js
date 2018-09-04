4e59b27f1ad04a9d771d74b4cf1f831568e66041
jamcha <jamcha.aa@gmail.com>
2018-09-04 14:32:05 +0900

    Mod toc

---
+++ .vuepress/summary.js
@@ -0,0 +1,32 @@
+exports.genSidebarConfig = function () {
+    return [
+            {
+                title: 'プロローグ',
+                children: ['01', '02']
+            },
+            {
+                title: '基本のキ',
+                children: ['03', '04', '05', '06', '07']
+            },
+            {
+                title: '処理ーン☆',
+                children: ['08', '09', '10', '11']
+            },
+            {
+                title: 'いつも覗かれている深淵，その縁',
+                children: ['12', '13', '14']
+            },
+            {
+                title: 'ラストスパート',
+                children: ['15', '16', '17', '18']
+            },
+            {
+                title: '私の最高のアシスタント',
+                children: ['19', '20', '21', '22']
+            },
+            {
+                title: 'エピローグ',
+                children: ['23', '24', '25']
+            }
+    ]
+}
